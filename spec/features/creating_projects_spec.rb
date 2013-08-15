2b7076a9bef4b675e78a180643e7e25e36e2c6e3
ghfuchs <george-fuchs@sbcglobal.net>
2013-08-15 09:06:56 -0500

    Commit 08/15/2013 -

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -16,10 +16,10 @@ feature 'Creating Projects' do
 
     project = Project.where(name: "TextMate 2").first
 
-        expect(page.current_url).to eql(project_url(project))
+    expect(page.current_url).to eql(project_url(project))
 
-        title = "TextMate 2 - Projects - Ticketee"
-        expect(find("title").native.text).to have_content(title)
+    title = "TextMate 2 - Projects - Ticketee"
+    expect(find("title").native.text).to have_content(title)
 
   end
 
